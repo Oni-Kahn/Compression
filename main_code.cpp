c845819b26951b98becbcfeddeabfa0a78643137
Oni-Kahn <kdelafu1@asu.edu>
2024-12-04 11:38:43 -0700

    Update main_code.cpp

--- main_code.cpp
+++ main_code.cpp
@@ -217,11 +217,12 @@ void plotMatrix(const vector<vector<double>>& matrix, const string& title)
 
   plt::figure();
   plt::title(title);
-  plt::show(block=True);
   plt::imshow(imageData.data(), height, width, 1, {{"cmap", "magma"}}); // 1 color channel for grayscale
   string filename = title + ".png";
   plt::save(filename);
   cout << "Plot saved as " << filename << endl;
+
+  plt::show(block=true);
 }
 
 // Function to process an 8x8 block of the image
